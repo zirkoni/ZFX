e93f34c18c630380a7b053ed6bdc2f726aa5f990
zirkoni <zirkoni88@hotmail.com>
2021-12-24 21:36:30 +0200

    Fix VS compilation

--- ZFX/include/colours.h
+++ ZFX/include/colours.h
@@ -1,14 +1,15 @@
+#pragma once
 #include <glm/glm.hpp>
 
 namespace ZFX
 {
-static constexpr glm::vec4 BLACK    { 0.0f, 0.0f, 0.0f, 1.0f };
-static constexpr glm::vec4 WHITE    { 1.0f, 1.0f, 1.0f, 1.0f };
-static constexpr glm::vec4 RED      { 1.0f, 0.0f, 0.0f, 1.0f };
-static constexpr glm::vec4 GREEN    { 0.0f, 1.0f, 0.0f, 1.0f };
-static constexpr glm::vec4 BLUE     { 0.0f, 0.0f, 1.0f, 1.0f };
-static constexpr glm::vec4 YELLOW   { 1.0f, 1.0f, 0.0f, 1.0f };
-static constexpr glm::vec4 L_GRAY   { 0.2f, 0.2f, 0.2f, 1.0f };
-static constexpr glm::vec4 D_GRAY   { 0.8f, 0.8f, 0.8f, 1.0f };
+static const glm::vec4 BLACK    { 0.0f, 0.0f, 0.0f, 1.0f };
+static const glm::vec4 WHITE    { 1.0f, 1.0f, 1.0f, 1.0f };
+static const glm::vec4 RED      { 1.0f, 0.0f, 0.0f, 1.0f };
+static const glm::vec4 GREEN    { 0.0f, 1.0f, 0.0f, 1.0f };
+static const glm::vec4 BLUE     { 0.0f, 0.0f, 1.0f, 1.0f };
+static const glm::vec4 YELLOW   { 1.0f, 1.0f, 0.0f, 1.0f };
+static const glm::vec4 L_GRAY   { 0.2f, 0.2f, 0.2f, 1.0f };
+static const glm::vec4 D_GRAY   { 0.8f, 0.8f, 0.8f, 1.0f };
 }
 
