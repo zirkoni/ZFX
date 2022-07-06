3f1bf189509138fa6bd09e616c143c7d89de2bd0
zirkoni <zirkoni88@hotmail.com>
2022-07-06 14:40:32 +0300

    Add cyan & magenta to constants
    Remove grays, use function instead to generate different shades of gray

--- ZFX/include/colours.h
+++ ZFX/include/colours.h
@@ -3,13 +3,25 @@
 
 namespace ZFX
 {
+// Only 0 or 1
 static const glm::vec4 BLACK    { 0.0f, 0.0f, 0.0f, 1.0f };
 static const glm::vec4 WHITE    { 1.0f, 1.0f, 1.0f, 1.0f };
 static const glm::vec4 RED      { 1.0f, 0.0f, 0.0f, 1.0f };
 static const glm::vec4 GREEN    { 0.0f, 1.0f, 0.0f, 1.0f };
 static const glm::vec4 BLUE     { 0.0f, 0.0f, 1.0f, 1.0f };
 static const glm::vec4 YELLOW   { 1.0f, 1.0f, 0.0f, 1.0f };
-static const glm::vec4 L_GRAY   { 0.2f, 0.2f, 0.2f, 1.0f };
-static const glm::vec4 D_GRAY   { 0.8f, 0.8f, 0.8f, 1.0f };
+static const glm::vec4 CYAN     { 0.0f, 1.0f, 1.0f, 1.0f };
+static const glm::vec4 MAGENTA  { 1.0f, 0.0f, 1.0f, 1.0f };
+
+// Grays
+glm::vec4 makeGray(float lightAmount)
+{
+    assert(lightAmount > 0.0f && lightAmount < 1.0f);
+
+    glm::vec4 colour = lightAmount * WHITE;
+    colour.a = 1.0f;
+    return colour;
+}
+
 }
 
